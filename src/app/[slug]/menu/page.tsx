8943ee24908d278636309f758414e7e04fff9beb
GuihCFerreira <guihhh231@gmail.com>
2025-02-20 21:04:49 -0300

    feat: add initial restaurant page and menu page

---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,39 @@
+import { notFound } from "next/navigation";
+
+import { getRestaurantBySlug } from "@/app/data/get-restaurant-by-slug";
+
+import RestaurantHeader from "./components/header";
+
+interface RestaurantsMenuPageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod: string }>;
+}
+
+const isConsumptionMethodValid = (consumptionMethod: string) => {
+  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
+};
+
+const RestaurantsMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantsMenuPageProps) => {
+  const { slug } = await params;
+  const { consumptionMethod } = await searchParams;
+
+  if (!isConsumptionMethodValid(consumptionMethod)) {
+    return notFound();
+  }
+
+  const restaurant = await getRestaurantBySlug(slug);
+
+  if (!restaurant) {
+    return notFound();
+  }
+  return (
+    <div>
+      <RestaurantHeader restaurant={restaurant} />
+    </div>
+  );
+};
+
+export default RestaurantsMenuPage;
