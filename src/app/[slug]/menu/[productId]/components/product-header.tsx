558827537afaf85648f75b6e3a26ab71ec011b7d
GuihCFerreira <guihhh231@gmail.com>
2025-02-23 11:49:11 -0300

    feat: add scroll to ingredients in product details page

--- src/app/[slug]/menu/[productId]/components/product-header.tsx
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -1,23 +1,23 @@
-"use client"
+"use client";
 
-import { Button } from "@/components/ui/button";
 import { Product } from "@prisma/client";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
+import { Button } from "@/components/ui/button";
+
 interface ProductHeaderProps {
-    product: Pick<Product, "imageUrl" | "name">;
+  product: Pick<Product, "imageUrl" | "name">;
 }
 
-const ProductHeader = ({product} : ProductHeaderProps) => {
-
-const router = useRouter();
+const ProductHeader = ({ product }: ProductHeaderProps) => {
+  const router = useRouter();
 
   const handleBackClick = () => router.back();
 
-    return ( 
-    <div className="relative w-full h-[300px]">
+  return (
+    <div className="relative min-h-[300px] w-full">
       <Button
         variant={"secondary"}
         className="absolute left-4 top-4 z-50 rounded-full"
@@ -26,16 +26,21 @@ const router = useRouter();
       >
         <ChevronLeftIcon />
       </Button>
-      <Image src={product.imageUrl} alt={product.name} fill className="object-contain" />
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        fill
+        className="object-contain"
+      />
       <Button
         variant={"secondary"}
         className="absolute right-4 top-4 z-50 rounded-full"
         size={"icon"}
       >
         <ScrollTextIcon />
-      </Button>        
-    </div> 
-    );
-}
- 
+      </Button>
+    </div>
+  );
+};
+
 export default ProductHeader;
