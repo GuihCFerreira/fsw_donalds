85f3aa221372245e54f2fc3fd8a7affc1807590e
GuihCFerreira <guihhh231@gmail.com>
2025-02-23 21:11:20 -0300

    feat: ensure order can be created

---
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -0,0 +1,67 @@
+"use server";
+
+import { ConsumptionMethod } from "@prisma/client";
+
+import { db } from "@/lib/prisma";
+
+import { removeCpfPunctuation } from "../helpers/cpf";
+
+interface CreateOrderInput {
+  costumerName: string;
+  costumerCpf: string;
+  products: Array<{
+    id: string;
+    quantity: number;
+  }>;
+  consumptionMethod: ConsumptionMethod;
+  slug: string;
+}
+
+export const createOrder = async (input: CreateOrderInput) => {
+  const restaurant = await db.restaurant.findFirst({
+    where: {
+      slug: input.slug,
+    },
+  });
+
+  if (!restaurant) {
+    throw new Error("Restaurant not found");
+  }
+
+  const productsWithPrices = await db.product.findMany({
+    where: {
+      id: {
+        in: input.products.map((product) => product.id),
+      },
+    },
+  });
+
+  if (productsWithPrices.length !== input.products.length) {
+    throw new Error("Product not found");
+  }
+
+  const productsWithPricesAndQuantities = input.products.map((product) => ({
+    productId: product.id,
+    quantity: product.quantity,
+    price: productsWithPrices.find((p) => p.id === product.id)!.price,
+  }));
+
+  await db.order.create({
+    data: {
+      consumptionMethod: input.consumptionMethod,
+      customerName: input.costumerName,
+      customerCpf: removeCpfPunctuation(input.costumerCpf),
+      status: "PENDING",
+      restaurantId: restaurant.id,
+      orderProducts: {
+        createMany: {
+          data: productsWithPricesAndQuantities,
+        },
+      },
+      total: productsWithPricesAndQuantities.reduce(
+        (acc, product) => acc + product.price * product.quantity,
+        0,
+      ),
+    },
+  });
+};
--- src/app/[slug]/menu/components/finish-order-dialog.tsx
+++ src/app/[slug]/menu/components/finish-order-dialog.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { ConsumptionMethod } from "@prisma/client";
+import { Loader2Icon } from "lucide-react";
+import { useParams, useSearchParams } from "next/navigation";
+import { useContext, useTransition } from "react";
 import { useForm } from "react-hook-form";
 import { PatternFormat } from "react-number-format";
+import { toast } from "sonner";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -25,6 +30,8 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
+import { createOrder } from "../actions/create-order";
+import { CartContext } from "../contexts/cart";
 import { isValidCpf } from "../helpers/cpf";
 
 const formSchema = z.object({
@@ -50,6 +57,11 @@ interface FinishOrderDialogProps {
 }
 
 const FinishOrderDialog = ({ open, onOpenChange }: FinishOrderDialogProps) => {
+    const searchParams = useSearchParams();
+    const { products } = useContext(CartContext);
+    const { slug } = useParams<{ slug: string }>();
+    const [isPending, startTransition] = useTransition();
+
     const form = useForm<FormSchema>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -59,11 +71,27 @@ const FinishOrderDialog = ({ open, onOpenChange }: FinishOrderDialogProps) => {
         shouldUnregister: true,
     });
 
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values);
-    }
+    const onSubmit = async (data: FormSchema) => {
+        try {
+            const consumptionMethod = searchParams.get("consumptionMethod") as ConsumptionMethod;
+
+            startTransition(async () => {
+                await createOrder({
+                    consumptionMethod,
+                    costumerCpf: data.cpf,
+                    costumerName: data.name,
+                    products,
+                    slug,
+                });
+
+                onOpenChange(false);
+                toast.success("Pedido finalizado com sucesso!");
+            })
+
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     return (
         <Drawer open={open} onOpenChange={onOpenChange}>
@@ -113,8 +141,10 @@ const FinishOrderDialog = ({ open, onOpenChange }: FinishOrderDialogProps) => {
                                     type="submit"
                                     variant="destructive"
                                     className="rounded-full w-full"
+                                    disabled={isPending}
                                 >
-                                    Finalizar
+                                    {isPending && <Loader2Icon className="animate-spin" />}
+                                    {isPending ? "Finalizando..." : "Finalizar"}
                                 </Button>
                                 <DrawerClose asChild>
                                     <Button variant="outline" className="rounded-full w-full">
